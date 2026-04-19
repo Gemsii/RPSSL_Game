9b339a08608e6adaac25a9c56d61a6077d50fffc
Tatjana Gemovic <tatjana.gemovic@icodefactory>
2026-04-20 00:10:36 +0200

    Refactor Program.cs class

---
+++ RPSSL/RPSSL.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,76 @@
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using RPSSL.API.Domain.Interfaces;
+using RPSSL.API.Features.Choices.GetChoices;
+using RPSSL.API.Features.Choices.GetRandomChoice;
+using RPSSL.API.Features.Games.Play;
+using RPSSL.API.Features.Players.Create;
+using RPSSL.API.Infrastructure.External;
+using RPSSL.API.Infrastructure.External.Options;
+using RPSSL.API.Infrastructure.Persistence.Configuration;
+using RPSSL.API.Infrastructure.Repositories;
+using RPSSL.API.Services;
+
+namespace RPSSL.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services)
+        {
+            services.AddDbContext<InMemoryDbContext>(options =>
+                options.UseInMemoryDatabase("RpSslDb"));
+
+            services.AddScoped<IGameRepository, GameRepository>();
+            services.AddScoped<IPlayerRepository, PlayerRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddExternalServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CodeChallengeApiOptions>(
+                configuration.GetSection(CodeChallengeApiOptions.SectionName));
+
+            services.AddHttpClient<IRandomNumberService, RandomNumberService>(client =>
+            {
+                client.BaseAddress = new Uri(configuration["CodeChallengeApi:BaseAddress"]!);
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IChoiceService, ChoiceService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddFeatures(this IServiceCollection services)
+        {
+            services.AddScoped<CreatePlayerHandler>();
+            services.AddScoped<IValidator<CreatePlayerCommand>, CreatePlayerCommandValidator>();
+
+            services.AddScoped<GetChoicesHandler>();
+            services.AddScoped<GetRandomChoiceHandler>();
+
+            services.AddScoped<PlayGameHandler>();
+            services.AddScoped<IValidator<PlayGameCommand>, PlayGameCommandValidator>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddCorsPolicy(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+            });
+
+            return services;
+        }
+    }
+}
--- RPSSL/RPSSL.API/Program.cs
+++ RPSSL/RPSSL.API/Program.cs
@@ -1,56 +1,21 @@
-using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using RPSSL.API.Domain.Interfaces;
 using RPSSL.API.Extensions;
-using RPSSL.API.Features.Choices.GetChoices;
-using RPSSL.API.Features.Choices.GetRandomChoice;
-using RPSSL.API.Features.Games.Play;
-using RPSSL.API.Features.Players.Create;
-using RPSSL.API.Infrastructure.External;
-using RPSSL.API.Infrastructure.External.Options;
-using RPSSL.API.Infrastructure.Persistence.Configuration;
-using RPSSL.API.Infrastructure.Repositories;
-using RPSSL.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services
+    .AddPersistence()
+    .AddExternalServices(builder.Configuration)
+    .AddApplicationServices()
+    .AddFeatures()
+    .AddCorsPolicy()
+    .AddControllers();
 
-builder.Services.AddDbContext<InMemoryDbContext>(options =>
-    options.UseInMemoryDatabase("RpSslDb"));
-
-builder.Services.Configure<CodeChallengeApiOptions>(
-    builder.Configuration.GetSection(CodeChallengeApiOptions.SectionName));
-
-builder.Services.AddHttpClient<IRandomNumberService, RandomNumberService>(client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["CodeChallengeApi:BaseAddress"]!);
-});
-
-builder.Services.AddScoped<IChoiceService, ChoiceService>();
-builder.Services.AddScoped<IGameRepository, GameRepository>();
-builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
-
-// Features
-builder.Services.AddScoped<CreatePlayerHandler>();
-builder.Services.AddScoped<IValidator<CreatePlayerCommand>, CreatePlayerCommandValidator>();
-builder.Services.AddScoped<GetChoicesHandler>();
-builder.Services.AddScoped<GetRandomChoiceHandler>();
-builder.Services.AddScoped<PlayGameHandler>();
-builder.Services.AddScoped<IValidator<PlayGameCommand>, PlayGameCommandValidator>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", policy =>
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader());
-});
 
 var app = builder.Build();
 
 app.Configure();
 
 app.Run();
+
